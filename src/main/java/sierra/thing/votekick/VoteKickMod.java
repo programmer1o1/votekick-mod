2e7b53410e837ba8e4ea4b10575bd64c45f108a8
programmer1o1 <rtrrt2996@gmail.com>
2025-08-26 12:04:13 +0800

    changed the version to 1.0.4 oopise

--- src/main/java/sierra/thing/votekick/VoteKickMod.java
+++ src/main/java/sierra/thing/votekick/VoteKickMod.java
@@ -30,7 +30,7 @@ import java.util.UUID;
 
 public class VoteKickMod implements ModInitializer {
     public static final String MOD_ID = "votekick";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.4";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     // Channel ID for checking if clients have the mod - kinda hacky but works
